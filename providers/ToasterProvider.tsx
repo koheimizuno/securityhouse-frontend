37d000baaf915c98bb76d1b005e95a83f56b6b2a
weijiezhang-star <144591405+weijiezhang-star@users.noreply.github.com>
2024-10-18 23:27:47 +0900

    customize the position of toaster

--- providers/ToasterProvider.tsx
+++ providers/ToasterProvider.tsx
@@ -4,7 +4,30 @@ import React from 'react'
 import { Toaster } from 'react-hot-toast'
 
 const ToasterProvider = () => {
-  return <Toaster />
+  return (
+    <Toaster
+      position='top-right'
+      toastOptions={{
+        style: {
+          marginTop: '20px',
+          marginRight: '20px',
+          padding: '10px 20px'
+        },
+        success: {
+          style: {
+            background: '#6cc070',
+            color: 'white'
+          }
+        },
+        error: {
+          style: {
+            background: 'red',
+            color: 'white'
+          }
+        }
+      }}
+    />
+  )
 }
 
 export default ToasterProvider
