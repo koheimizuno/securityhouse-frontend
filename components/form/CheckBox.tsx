66f8d5a8e2e5bf95c916c85ab460780870d43207
weijiezhang-star <144591405+weijiezhang-star@users.noreply.github.com>
2024-10-18 23:27:05 +0900

    create checkbox component

--- components/form/CheckBox.tsx
+++ components/form/CheckBox.tsx
@@ -1,11 +1,12 @@
 type CheckBoxProps = {
+  name: string
   on: boolean
-  handleToggle: () => void
+  handleToggle: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
-const CheckBox = ({ on, handleToggle }: CheckBoxProps) => {
+const CheckBox = ({ name, on, handleToggle }: CheckBoxProps) => {
   return (
     <label className='inline-flex items-center cursor-pointer'>
-      <input type='checkbox' name='news_not' className='sr-only peer' checked={on} onChange={handleToggle} />
+      <input type='checkbox' name={name} className='sr-only peer' checked={on} onChange={handleToggle} />
       <div className="relative w-11 h-6 bg-gray-300 rounded-full peer peer-checked:bg-blue-600 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
     </label>
   )
