bddca5cfa11882fe2cf3dc0216bb5313f04e9279
weijiezhang-star <skygentleman626@gmail.com>
2024-10-31 00:51:06 +0900

    implement scrollToRef in document office-store page

--- app/document/sales-office/proposal/office-store/page.tsx
+++ app/document/sales-office/proposal/office-store/page.tsx
@@ -1,45 +1,74 @@
 'use client'
 
+import { useRef } from 'react'
+import Image from 'next/image'
+
 import PageHeader from '@/components/common/PageHeader'
 import SectionTitle from '@/components/common/SectionTitle'
 import Container from '@/components/layout/Container'
 import DocCardButton from '@/views/document/DocCardButton'
 import DocumentCard from '@/views/document/DocumentCard'
 import { Divider } from '@nextui-org/react'
-import Image from 'next/image'
 
 const DocummentOfficeStorePage = () => {
+  const officeRef = useRef<HTMLDivElement>(null)
+  const storeRef = useRef(null)
+  const robberyRef = useRef(null)
+  const proposalRef = useRef<HTMLDivElement>(null)
+
+  const scrollToRef = (ref: React.RefObject<HTMLDivElement>, offset = 150) => {
+    if (ref.current) {
+      const elementPosition = ref.current.getBoundingClientRect().top + window.scrollY
+      window.scrollTo({
+        top: elementPosition - offset,
+        behavior: 'smooth'
+      })
+    }
+  }
+
   return (
     <Container className='py-16 flex flex-col gap-8'>
       <div className='flex items-start gap-10'>
         <ul className='w-[205px] hidden md:block'>
-          <li className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 cursor-pointer'>
-            <span>事務所向け</span>
+          <li
+            className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 cursor-pointer'
+            onClick={() => scrollToRef(officeRef)}
+          >
+            <span className='text-sm'>事務所向け</span>
             <Image src='/images/icons/arrow-bottom.svg' alt='arrow-bottom.svg' width={12} height={12} />
           </li>
-          <li className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 border-t-0 cursor-pointer'>
-            <span>店舗向け</span>
+          <li
+            className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 border-t-0 cursor-pointer'
+            onClick={() => scrollToRef(storeRef)}
+          >
+            <span className='text-sm'>店舗向け</span>
             <Image src='/images/icons/arrow-bottom.svg' alt='arrow-bottom.svg' width={12} height={12} />
           </li>
-          <li className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 border-t-0 cursor-pointer'>
-            <span>強盗対策</span>
+          <li
+            className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 border-t-0 cursor-pointer'
+            onClick={() => scrollToRef(robberyRef)}
+          >
+            <span className='text-sm'>強盗対策</span>
             <Image src='/images/icons/arrow-bottom.svg' alt='arrow-bottom.svg' width={12} height={12} />
           </li>
-          <li className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 border-t-0 cursor-pointer'>
-            <span>提案書</span>
+          <li
+            className='h-12 px-4 flex justify-between items-center bg-colorGray1 border border-colorGray3 border-t-0 cursor-pointer'
+            onClick={() => scrollToRef(proposalRef)}
+          >
+            <span className='text-sm'>提案書</span>
             <Image src='/images/icons/arrow-bottom.svg' alt='arrow-bottom.svg' width={12} height={12} />
           </li>
         </ul>
         <div className='w-full md:w-[calc(100%-246px)] flex flex-col gap-8'>
           <PageHeader title='営業・事務' subtitle='資料集' />
-          <section className='flex flex-col gap-6'>
+          <section className='flex flex-col gap-6' ref={officeRef}>
             <SectionTitle title='事務所向け' bar={true} divider={true} />
             <div className='flex flex-col md:flex-row md:flex-wrap items-center gap-4'>
               <DocumentCard title='守り方改革チラシ.pdf(000KB)' img='/images/doc-office-img01.png' />
               <DocumentCard title='選挙事務所対応.pdf(000KB)' img='/images/doc-office-img02.png' />
             </div>
           </section>
-          <section className='flex flex-col gap-6'>
+          <section className='flex flex-col gap-6' ref={storeRef}>
             <SectionTitle title='店舗向け' bar={true} divider={true} />
             <div className='flex flex-col md:flex-row md:flex-wrap items-center gap-4'>
               <DocumentCard title='住居併用型向自主機械警備.pdf(000KB)' img='/images/doc-store-img01.png' />
@@ -47,13 +76,13 @@ const DocummentOfficeStorePage = () => {
               <DocumentCard title='調剤薬局／ドラッグストア.pdf(000KB)' img='/images/doc-store-img03.png' />
             </div>
           </section>
-          <section className='flex flex-col gap-6'>
+          <section className='flex flex-col gap-6' ref={robberyRef}>
             <SectionTitle title='強盗対策' bar={true} divider={true} />
             <div className='flex flex-col md:flex-row md:flex-wrap items-center gap-4'>
               <DocumentCard title='強盗・不審者対策.pdf(000KB)' img='/images/doc-robbery-img01.png' />
             </div>
           </section>
-          <section className='flex flex-col gap-6'>
+          <section className='flex flex-col gap-6' ref={proposalRef}>
             <SectionTitle title='提案書' bar={true} divider={true} />
             <div className='flex justify-between flex-wrap gap-8'>
               <h4 className='md:py-4 md:max-w-[220px]'>KDDI様向けi-NEXTのご提案</h4>
