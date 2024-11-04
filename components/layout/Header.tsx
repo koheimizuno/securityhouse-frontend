d5c7ecf270bf4e8b9d3b8452468a1a3b8d28ebec
weijiezhang-star <skygentleman626@gmail.com>
2024-11-04 20:45:19 +0900

    fix login button without authentication

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,10 +7,12 @@ import { usePathname } from 'next/navigation'
 
 import { Button } from '@nextui-org/react'
 import { isDisplayHeaderPage } from '@/utils/isPublicPage'
+import { useAuthentication } from '@/hooks/AuthContext'
 
 const Header = () => {
   const pathname = usePathname()
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
+  const { isAuthenticated } = useAuthentication()
   const isDisplayHeader = isDisplayHeaderPage(pathname)
 
   const handleHamburger = () => {
@@ -22,7 +24,7 @@ const Header = () => {
         <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
           <div className='flex flex-col items-start gap-0 md:flex-row md:items-center md:gap-2'>
             <Link href='/'>
-              <Image src='/images/icons/logo.svg' className='w-[217px] h-[40px]' alt='Logo' width={217} height={40} />
+              <Image src='/images/icons/logo.svg' className='object-contain' alt='Logo' width={217} height={40} />
             </Link>
             <Link href='/sh-club' className='text-[15px] font-bold text-[#333] ps-2 md:ps-0'>
               SH会員専用ページ
@@ -33,33 +35,54 @@ const Header = () => {
               isHamburgerOpen ? 'max-h-[1000px] overflow-visible py-8' : 'max-h-0 overflow-hidden'
             }`}
           >
-            <Link href='/chatroom/post/bookmark/'>
-              <Button
-                className='rounded-full w-[200px]'
-                startContent={
-                  <Image
-                    src='/images/icons/bookmark-icon-black.svg'
-                    alt='bookmark-icon-black'
-                    className='w-5 h-5'
-                    width={16}
-                    height={16}
-                  />
-                }
-              >
-                ブックマーク
-              </Button>
-            </Link>
-            <Link href='/profile/1'>
-              <Button
-                color='primary'
-                className='rounded-full w-[200px]'
-                startContent={
-                  <Image src='/images/icons/user-icon.svg' alt='user-icon' className='w-5 h-5' width={16} height={16} />
-                }
-              >
-                マイページ
-              </Button>
-            </Link>
+            {isAuthenticated ? (
+              <>
+                <Link href='/chatroom/post/bookmark/'>
+                  <Button
+                    className='rounded-full w-[200px]'
+                    startContent={
+                      <Image
+                        src='/images/icons/bookmark-icon-black.svg'
+                        alt='bookmark-icon-black'
+                        className='w-5 h-5'
+                        width={16}
+                        height={16}
+                      />
+                    }
+                  >
+                    ブックマーク
+                  </Button>
+                </Link>
+                <Link href='/profile/1'>
+                  <Button
+                    color='primary'
+                    className='rounded-full w-[200px]'
+                    startContent={
+                      <Image
+                        src='/images/icons/user-icon.svg'
+                        alt='user-icon'
+                        className='w-5 h-5'
+                        width={16}
+                        height={16}
+                      />
+                    }
+                  >
+                    マイページ
+                  </Button>
+                </Link>
+              </>
+            ) : (
+              <Link href='/login'>
+                <Button
+                  className='rounded-full w-[200px]'
+                  startContent={
+                    <Image src='/images/icons/login.svg' alt='login' className='w-5 h-5' width={16} height={16} />
+                  }
+                >
+                  マイページ
+                </Button>
+              </Link>
+            )}
           </div>
           <div className={`${isHamburgerOpen ? 'change' : ''} sm:hidden`} onClick={handleHamburger}>
             <div className='bar1'></div>
