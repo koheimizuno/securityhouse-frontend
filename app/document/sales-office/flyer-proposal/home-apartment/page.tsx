73d0d297eed63ae12abc216b60ac71079d0c2d4d
koheimizuno <skygentleman626@gmail.com>
2024-11-19 23:12:16 +0900

    created document home-apartment page and integrated api

---
+++ app/document/sales-office/flyer-proposal/home-apartment/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import PageHeader from '@/components/common/PageHeader'
+import Container from '@/components/layout/Container'
+import DocumentCard from '@/views/document/DocumentCard'
+
+import { getDocumentsAction } from '@/actions/documentAction'
+import { useAuthentication } from '@/hooks/AuthContext'
+import { DocumentType } from '@/types/documentType'
+
+const DocumentHomeApartmentPage = () => {
+  const { session_user_id } = useAuthentication()
+  const [homeApartmentDocData, setHomeApartmentDocData] = useState<DocumentType[] | null>([])
+
+  useEffect(() => {
+    getDocumentsAction({ type_id: 1, category_id: 9, user_id: session_user_id }).then(data =>
+      setHomeApartmentDocData(data)
+    )
+  }, [session_user_id])
+
+  return (
+    <Container className='py-16 flex flex-col gap-12'>
+      <PageHeader title='一般家庭・マンション向け' subtitle='資料集' />
+      <section className='flex flex-col gap-6'>
+        <div className='flex flex-col md:flex-row md:flex-wrap items-center gap-4'>
+          {homeApartmentDocData &&
+            homeApartmentDocData.map((doc, key) => (
+              <DocumentCard key={key} title={doc.title} img={doc.image} attachment={doc.attachment} />
+            ))}
+        </div>
+      </section>
+    </Container>
+  )
+}
+
+export default DocumentHomeApartmentPage
