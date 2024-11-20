bf36a652bddb524ccdd37c69fb8fc5145e90063d
koheimizuno <skygentleman626@gmail.com>
2024-11-20 10:26:46 +0900

    fix apis related to category

--- redux-store/slices/categorySlice.ts
+++ redux-store/slices/categorySlice.ts
@@ -1,18 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios'
-import toast from 'react-hot-toast'
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 import { CategoryType } from '@/types/categoryType'
 import { storeInitialType } from '@/types/storeInitialType'
-
-export const createCategoryAction: any = createAsyncThunk('createCategoryAction', async (payload: CategoryType) => {
-  try {
-    await axios.post(`/api/category/`, payload)
-  } catch (err: any) {
-    return err.response.data
-  }
-})
 
 export const getCategoryAction: any = createAsyncThunk(
   'getCategoryAction',
@@ -30,22 +21,6 @@ export const getCategoryAction: any = createAsyncThunk(
     }
   }
 )
-
-export const editCategoryAction: any = createAsyncThunk('editCategoryAction', async (payload: CategoryType) => {
-  try {
-    await axios.put(`/api/category/`, payload)
-  } catch (err: any) {
-    return err.response.data
-  }
-})
-
-export const deleteCategoryAction: any = createAsyncThunk('deleteCategoryAction', async (id: string) => {
-  try {
-    await axios.delete(`/api/category/`, { params: { id: id } })
-  } catch (err: any) {
-    return err.response.data
-  }
-})
 
 interface initialStateTypes extends storeInitialType {
   categories: CategoryType[]
@@ -75,45 +50,6 @@ export const categorySlice = createSlice({
       .addCase(getCategoryAction.rejected, state => {
         state.isLoading = false
         state.error = true
-      })
-      .addCase(createCategoryAction.pending, state => {
-        state.isLoading = true
-      })
-      .addCase(createCategoryAction.fulfilled, state => {
-        state.isLoading = false
-        state.success = true
-        toast.success('カテゴリーが正常に作成されました。')
-      })
-      .addCase(createCategoryAction.rejected, (state, { payload }) => {
-        state.isLoading = false
-        state.error = true
-        toast.error(payload.message)
-      })
-      .addCase(editCategoryAction.pending, state => {
-        state.isLoading = true
-      })
-      .addCase(editCategoryAction.fulfilled, state => {
-        state.isLoading = false
-        state.success = true
-        toast.success('カテゴリーが正常に変更されました。')
-      })
-      .addCase(editCategoryAction.rejected, (state, { payload }) => {
-        state.isLoading = false
-        state.error = true
-        toast.error(payload.message)
-      })
-      .addCase(deleteCategoryAction.pending, state => {
-        state.isLoading = true
-      })
-      .addCase(deleteCategoryAction.fulfilled, state => {
-        state.isLoading = false
-        state.success = true
-        toast.success('カテゴリーが正常に削除されました。')
-      })
-      .addCase(deleteCategoryAction.rejected, (state, { payload }) => {
-        state.isLoading = false
-        state.error = true
-        toast.error(payload.message)
       })
   }
 })
