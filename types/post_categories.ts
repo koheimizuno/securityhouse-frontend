31a77894072253870ecc8b35a2aa5ceadfbab402
NOSH\risasuzuki <suzuki.r.cat0130@gmail.com>
2024-12-13 16:16:34 +0900

    投稿カテゴリー一覧を追加

---
+++ types/post_categories.ts
@@ -0,0 +1,9 @@
+export interface post_categories {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+  type_id: number;
+}
