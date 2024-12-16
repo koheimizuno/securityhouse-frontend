0025e3ee245907f00d0c588277ed392c9d1dc004
sakamura_nosh <narumi.sakamura@nosh-inc.com>
2024-12-16 20:07:30 +0900

    ランキング一覧追加

---
+++ app/admin/ranking/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Container from '@/components/layout/Container';
+import Sidemenu from '@/components/admin/SideMenu';
+import Table from '@/components/admin/Table';
+import ButtonSet from '@/components/admin/ButtonSet';
+import { useSearchableData } from '@/actions/adminAction';
+
+const Ranking = () => {
+    const {
+      data,
+      currentPage,
+      totalPages,
+      onSearch,
+      setCurrentPage,
+    } = useSearchableData('http://localhost:4000/rankings/', ['title'], 0);
+  
+  
+    return (
+      <Container className="py-16 flex flex-col gap-12">
+        <div className='flex relative'>
+            <Sidemenu />
+            <div className="w-full">
+                <ButtonSet
+                    onSearchSubmit={onSearch}
+                    links={[
+                        { label: '新規登録', href: '/create' },
+                    ]}
+                />
+                <Table
+                    columns={[
+                        { key: 'title', label: 'タイトル' },
+                        { key: 'points', label: 'ポイント'}
+                    ]}
+                    data={data}
+                    baseUrl="/ranking"
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    setCurrentPage={setCurrentPage}
+                />
+            </div>
+        </div>
+      </Container>
+    );
+  };
+  
+  export default Ranking;
---
+++ types/rankingDetailType.ts
@@ -0,0 +1,8 @@
+export interface rankingDetailType {
+  id: number;
+  title: string;
+  points: number;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+}
