3991a31340bd954d9ef884458fa808a0bb016153
NOSH\risasuzuki <suzuki.r.cat0130@gmail.com>
2024-12-17 16:05:40 +0900

    お知らせページ追加

---
+++ app/admin/news/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Container from '@/components/layout/Container';
+import Sidemenu from '@/components/admin/SideMenu';
+import Table from '@/components/admin/Table';
+import ButtonSet from '@/components/admin/ButtonSet';
+import { useSearchableData } from '@/actions/adminAction';
+
+const News = () => {
+    const {
+      data,
+      currentPage,
+      totalPages,
+      onSearch,
+      setCurrentPage,
+    } = useSearchableData('http://localhost:4000/news', ['title'], 0);
+  
+  
+    return (
+      <Container className="py-16 flex flex-col gap-12">
+        <div className='flex relative'>
+            <Sidemenu />
+            <div className="w-full">
+                <ButtonSet
+                    onSearchSubmit={onSearch}
+                    links={[
+                        { label: '新規登録', href: '/create' },
+                    ]}
+                />
+                <Table
+                    columns={[
+                        { key: 'name', label: 'タイトル' },
+                    ]}
+                    data={data}
+                    baseUrl="/news"
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    setCurrentPage={setCurrentPage}
+                />
+            </div>
+        </div>
+      </Container>
+    );
+  };
+  
+  export default News;
