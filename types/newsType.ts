e5f8333bd5da2c74c41248db3d86fbe552532bc1
NOSH\risasuzuki <suzuki.r.cat0130@gmail.com>
2024-12-17 17:44:16 +0900

    お知らせ一覧修正

--- types/newsType.ts
+++ types/newsType.ts
@@ -2,16 +2,9 @@ export type NewsType = {
   id: number
   title: string
   content: string
-  attachment: string
-  category_id: string
-  category_name: string
-  user_id: string
-  name: string
-  user_name: string
-  affiliation_name: string
-  thumbnail: string
-  nice_flag: boolean
-  like_count: number
-  bookmark_flag: boolean
-  created_at: string
+  category_at: string
+  updated_at: string
+  deleted_at: string
+  created_id: number
+  user_id: number
 }
