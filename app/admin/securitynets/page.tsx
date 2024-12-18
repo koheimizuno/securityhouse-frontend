729f9d38ed51330c9e606d6cea1fd2b647fcc68d
NOSH\risasuzuki <suzuki.r.cat0130@gmail.com>
2024-12-18 11:45:38 +0900

    セキュリティネットページ追加

---
+++ app/admin/securitynets/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Container from '@/components/layout/Container';
+import Sidemenu from '@/components/admin/SideMenu';
+import Table from '@/components/admin/Table';
+import ButtonSet from '@/components/admin/ButtonSet';
+import { useSearchableData } from '@/actions/adminAction';
+
+const Securitynets = () => {
+    const {
+      data,
+      currentPage,
+      totalPages,
+      onSearch,
+      setCurrentPage,
+    } = useSearchableData('http://localhost:4000/securitynets', ['title'], 0);
+  
+  
+    return (
+      <Container className="py-16 flex flex-col gap-12">
+        <div className='flex relative'>
+            <Sidemenu />
+            <div className="w-full">
+                <ButtonSet
+                    onSearchSubmit={onSearch}
+                    links={[
+                        { label: '新規登録', href: '/create' },
+                    ]}
+                />
+                <Table
+                    columns={[
+                        { key: 'title', label: 'タイトル' },
+                    ]}
+                    data={data}
+                    baseUrl="/securitynets"
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    setCurrentPage={setCurrentPage}
+                />
+            </div>
+        </div>
+      </Container>
+    );
+  };
+  
+  export default Securitynets;
---
+++ types/securitynetsType.ts
@@ -0,0 +1,9 @@
+export type NewsType = {
+  id: number
+  title: string
+  code: string
+  user_id: number
+  category_at: string
+  updated_at: string
+  deleted_at: string
+}
